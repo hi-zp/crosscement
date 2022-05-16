0162518c7264e98f02aba018c594d744c2fff82f
hi-zp <2915938439@qq.com>
2022-05-16 10:35:12 +0800

    fix: tooltip demo

--- apps/example/App.tsx
+++ apps/example/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import StorybookUIRoot from './.ondevice/Storybook';
 import { Platform, StatusBar, View } from 'react-native';
 import { DropdownProdiver } from '@crosscement/react-native-dropdown';
+import { TooltipProdiver } from '@crosscement/react-native-tooltip';
 
 export default () => (
   <DropdownProdiver>
-    <View
-      style={{
-        flex: 1,
-        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-      }}
-    >
-      <StorybookUIRoot />
-    </View>
+    <TooltipProdiver>
+      <View
+        style={{
+          flex: 1,
+          marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        }}
+      >
+        <StorybookUIRoot />
+      </View>
+    </TooltipProdiver>
   </DropdownProdiver>
 );
